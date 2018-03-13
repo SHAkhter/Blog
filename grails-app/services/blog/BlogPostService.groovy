6b30d056480059ad30228fa4ab0a1a55605690f9
Saife Akhter <sakhter@manifestcorp.com>
2018-03-13 15:56:29 -0400

    Improved listing functionality

---
+++ grails-app/services/blog/BlogPostService.groovy
@@ -0,0 +1,18 @@
+package blog
+
+import grails.gorm.services.Service
+
+@Service(BlogPost)
+interface BlogPostService {
+
+    BlogPost get(Serializable id)
+
+    List<BlogPost> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    BlogPost save(BlogPost blogPost)
+
+}
